16884a2af6c4ae59559b96755f136ba78281d2be
Isaac Smith <isaachs2@illinois.edu>
2018-02-08 10:32:38 -0600

    Add ViewModel function, GetMenus self executing

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,25 @@
-// Perform the fetch
-fetch(
-  "http://comm-data-dev.beckman.illinois.edu/api/v1/feeds/cafemenus?style=html"
-)
-  .then(res => {
-    return res.json();
-  })
-  .then(data => {
-    data.forEach(element => {
-      $("<div>")
-        .attr("class", "menuItem")
-        .appendTo("#menus")
-        .html(element.Description);
+function DailyMenuViewModel() {
+
+}
+
+(function GetMenus() {
+  // Perform the fetch
+  fetch(
+    "http://comm-data-dev.beckman.illinois.edu/api/v1/feeds/cafemenus?style=html"
+  )
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      data.forEach(element => {
+        $("<div>")
+          .attr("class", "menuItem")
+          .appendTo("#menus")
+          .html(element.Description);
+      });
+    })
+    .catch(function(message) {
+      console.log("ERROR: " + toString(message));
     });
-  })
-  .catch(function(message) {
-    console.log("ERROR: " + toString(message));
-  });
-console.log("Howdy!");
+  console.log("Howdy!");
+})();
