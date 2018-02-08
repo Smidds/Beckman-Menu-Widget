4ce75fbb9be9dfaad1e1702694630da32f1d8a15
Isaac Smith <isaachs2@illinois.edu>
2018-02-08 14:32:14 -0600

    Establish 3 second loop on reload

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,21 @@
-/**
- * Gets the daily specialty menus from the Beckman Comms DEV API
- *
- * @return
- *  The specialty menus in JSON format
- */
-function GetMenus() {
-  // Perform the fetch
+function Menu(Title, DateTime, Description) {
   var self = this;
-  self.menus = ko.observableArray();
-  fetch(
-    "http://comm-data-dev.beckman.illinois.edu/api/v1/feeds/cafemenus?style=unicode"
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(json => {
-      console.log("Loading...");
-      MakeMenus(json);
-    })
-    .catch(function(message) {
-      console.log("ERROR: " + toString(message));
-    });
+  self.title = ko.observable(Title);
+  self.date = ko.observable(DateTime);
+  self.desc = ko.observable(Description);
 }
 
 function MakeMenus(json) {
+  var self = this;
   // Here we need to go through the elements of the JSON and create objects
-  self.menus(json);
+  var len = json.length;
+  menusArray = [];
+  for (var i = 0; i < len; i++) {
+    var dayMenu = json[i];
+    menusArray.push(new Menu(dayMenu.Title, dayMenu.Date, dayMenu.Description));
+  }
+  return menusArray;
+  //return menusArray; // Async is causing this to fail
 }
 
 /**
@@ -33,8 +23,19 @@ function MakeMenus(json) {
  */
 function DailyMenuViewModel() {
   var self = this;
-  GetMenus();
-  //setInterval(GetMenus, 30000);
+  self.menus = ko.observableArray([]);
+  (function getMenus(){
+    fetch(
+      "http://comm-data-dev.beckman.illinois.edu/api/v1/feeds/cafemenus?style=unicode"
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        self.menus(MakeMenus(json));
+      });
+    setTimeout(getMenus, 3000);
+  })();
 }
 
 ko.applyBindings(new DailyMenuViewModel());
