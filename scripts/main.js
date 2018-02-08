c6aac381ddb56aa0a0a0fcba85d2d9bf2b5b652e
Isaac Smith <isaachs2@illinois.edu>
2018-02-08 10:24:42 -0600

    Move main work to main.js

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+// Perform the fetch
+fetch(
+  "http://comm-data-dev.beckman.illinois.edu/api/v1/feeds/cafemenus?style=html"
+)
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    data.forEach(element => {
+      $("<div>")
+        .attr("class", "menuItem")
+        .appendTo("#menus")
+        .html(element.Description);
+    });
+  })
+  .catch(function(message) {
+    console.log("ERROR: " + toString(message));
+  });
+console.log("Howdy!");
