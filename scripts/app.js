24e3cab083da50500e1bc8b6410ce155605d07ab
Isaac Smith <isaachs2@illinois.edu>
2018-02-13 10:30:10 -0600

    Configure to paths

--- scripts/app.js
+++ scripts/app.js
@@ -25,15 +25,6 @@ function DailyMenuViewModel() {
   var self = this;
   self.menus = ko.observableArray([]);
   (function getMenus(){
-    // fetch(
-    //   "http://comm-data-dev.beckman.illinois.edu/api/v1/feeds/cafemenus?style=html"
-    // )
-    //   .then(response => {
-    //     return response.json();
-    //   })
-    //   .then(json => {
-    //     self.menus(MakeMenus(json));
-    //   });
     $.getJSON("http://comm-data-dev.beckman.illinois.edu/api/v1/feeds/cafemenus?style=html", function(response){
       self.menus(MakeMenus(response));
     });
@@ -42,7 +33,3 @@ function DailyMenuViewModel() {
 }
 
 ko.applyBindings(new DailyMenuViewModel());
-
-(function test() {
-  
-})();
