f665bb72d8b708adeb0df6365c3ac892d38ac4e7
Isaac Smith <isaachs2@illinois.edu>
2018-02-15 16:43:29 -0600

    Add in the node version of signalr

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+ //'http://comm-data-dev.beckman.illinois.edu';
+var connection = $.hubConnection();
+var cafeMenuProxy = connection.createHubProxy('cafeMenu');
+
+connection.url = 'http://localhost:50345/signalr'
+connection.start()
+  .done(() => {
+    console.log('Now connected, connection ID=' + $.connection.hub.id)
+  })
+  .fail((error) => 
+    {console.log('Failure to connect! Error: ' + typeof(error))
+  });
