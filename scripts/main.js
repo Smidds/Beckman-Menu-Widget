ec1ce87cc26c7c8668e461e31bad826deaf3bb20
Isaac Smith <isaachs2@illinois.edu>
2018-02-20 08:41:00 -0600

    Fix connection readout

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ var cafeMenuProxy = connection.createHubProxy('cafeMenu');
 connection.url = 'http://localhost:50345/signalr'
 connection.start()
   .done(() => {
-    console.log('Now connected, connection ID=' + $.connection.hub.id)
+    console.log('Now connected, connection ID=' + connection.id)
   })
   .fail((error) => 
     {console.log('Failure to connect! Error: ' + typeof(error))
